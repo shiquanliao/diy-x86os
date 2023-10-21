4bc94268e7d5d038e62fe43b530b28a9eb00bab0
stone <sqcoding@163.com>
2023-10-21 14:25:51 +0800

    解释 qemu -s -S 的参数

--- diy-200lines-os/b00.00/script/qemu-debug-osx.sh
+++ diy-200lines-os/b00.00/script/qemu-debug-osx.sh
@@ -1,2 +1,5 @@
 # 适用于mac
+# 解释：-s -S 用于gdb调试
+# -s shorthand for -gdb tcp::1234, i.e. open a gdbserver on TCP port 1234
+# -S do not start CPU at startup (you must type 'c' in the monitor). 
 qemu-system-i386 -m 128M -s -S  -drive file=disk.img,index=0,media=disk,format=raw
