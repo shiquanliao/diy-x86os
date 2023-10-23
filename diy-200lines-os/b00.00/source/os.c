7cff2839af4b428f23d322dcb52c06fb812be9c3
stone <sqcoding@163.com>
2023-10-23 14:23:23 +0800

    进入32位保护模式

--- diy-200lines-os/b00.00/source/os.c
+++ diy-200lines-os/b00.00/source/os.c
@@ -8,3 +8,18 @@
  *课程请见：https://study.163.com/course/introduction.htm?courseId=1212765805&_trace_c_p_k2_=0bdf1e7edda543a8b9a0ad73b5100990
  */
 #include "os.h"
+
+// 定义类型
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+
+struct
+{
+    uint16_t limit_l, base_l, basehl_attr, base_limit;
+} gdt_table[256] __attribute__((aligned(8))) = {
+    // 0x00cf9a000000ffff - 从0地址开始，P存在，DPL=0，Type=非系统段，32位代码段（非一致代码段），界限4G，
+    [KERNEL_CODE_SEG / 8] = {0xffff, 0x0000, 0x9a00, 0x00cf},
+    // 0x00cf93000000ffff - 从0地址开始，P存在，DPL=0，Type=非系统段，数据段，界限4G，可读写
+    [KERNEL_DATA_SEG / 8] = {0xffff, 0x0000, 0x9200, 0x00cf},
+};
