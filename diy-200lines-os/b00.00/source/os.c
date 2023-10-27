4eeced2ea82c722f54ef1ed7fd6c3db490771226
stone <sqcoding@163.com>
2023-10-27 21:13:00 +0800

    任务切换--未完成

--- diy-200lines-os/b00.00/source/os.c
+++ diy-200lines-os/b00.00/source/os.c
@@ -9,6 +9,7 @@
  */
 #include "os.h"
 
+// --------------------------------- Macro Define ---------------------------------
 /**
  * @brief 系统页表
  * 下面配置中只做了一个处理，即将0x0-4MB虚拟地址映射到0-4MB的物理地址，做恒等映射。
@@ -19,21 +20,90 @@
 #define PDE_U (1 << 2)        // 页表用户态
 #define PDE_PS (1 << 7)       // 页表4MB页
 
-// 定义类型
+// -------------------------------- Type Define --------------------------------
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 
+// --------------------------------- External function Define ---------------------------------
+// timer_init: 初始化定时器, 函数声明
+void timer_init(void);
+void syscall_handler(void);
+
+// --------------------------------- Global variable Define ---------------------------------
 uint8_t map_phy_buffer[4096] __attribute__((aligned(4096))) = {0x36};     // align 4096: 4KB对齐, map_phy_buffer是一个页表，页表大小为4KB
-
 static uint32_t pte_table[1024] __attribute__((aligned(4096))) = {PDE_U}; // 要给个值，否则其实始化值不确定
-
 uint32_t pde_table[1024] __attribute__((aligned(4096))) = {
     [0] = (0) | PDE_P | PDE_PS | PDE_W | PDE_U, // PDE_PS，开启4MB的页，恒等映射
 };
 
 // 用户态任务的栈
-uint32_t task_dpl3_stack[1024];
+uint32_t task0_dpl0_stack[1024], task0_dpl3_stack[1024],
+    task1_dpl0_stack[1024], task1_dpl3_stack[1024];
+
+uint32_t task0_tss[] = {
+    // prelink, esp0, ss0, esp1, ss1, esp2, ss2
+    0,
+    (uint32_t)task0_dpl0_stack + 4 * 1024,
+    KERNEL_DATA_SEG,
+    /* 后边不用使用 */ 0x0,
+    0x0,
+    0x0,
+    0x0,
+    // cr3, eip, eflags, eax, ecx, edx, ebx, esp, ebp, esi, edi,
+    (uint32_t)pg_dir,
+    (uint32_t)task_0 /*入口地址*/,
+    0x202,
+    0xa,
+    0xc,
+    0xd,
+    0xb,
+    (uint32_t)task1_dpl0_stack + 4 * 1024 /* 栈 */,
+    0x1,
+    0x2,
+    0x3,
+    // es, cs, ss, ds, fs, gs, ldt, iomap
+    APP_DATA_SEG,
+    APP_CODE_SEG,
+    APP_DATA_SEG,
+    APP_DATA_SEG,
+    APP_DATA_SEG,
+    APP_DATA_SEG,
+    0x0,
+    0x0,
+};
+
+uint32_t task1_tss[] = {
+    // prelink, esp0, ss0, esp1, ss1, esp2, ss2
+    0,
+    (uint32_t)task1_dpl0_stack + 4 * 1024,
+    KERNEL_DATA_SEG,
+    /* 后边不用使用 */ 0x0,
+    0x0,
+    0x0,
+    0x0,
+    // cr3, eip, eflags, eax, ecx, edx, ebx, esp, ebp, esi, edi,
+    (uint32_t)pg_dir,
+    (uint32_t)task_1 /*入口地址*/,
+    0x202,
+    0xa,
+    0xc,
+    0xd,
+    0xb,
+    (uint32_t)task1_dpl3_stack + 4 * 1024 /* 栈 */,
+    0x1,
+    0x2,
+    0x3,
+    // es, cs, ss, ds, fs, gs, ldt, iomap
+    APP_DATA_SEG,
+    APP_CODE_SEG,
+    APP_DATA_SEG,
+    APP_DATA_SEG,
+    APP_DATA_SEG,
+    APP_DATA_SEG,
+    0x0,
+    0x0,
+};
 
 // ldt_table: 局部描述符表
 struct
@@ -50,32 +120,93 @@ struct
     [KERNEL_CODE_SEG / 8] = {0xffff, 0x0000, 0x9a00, 0x00cf},
     // 0x00cf93000000ffff - 从0地址开始，P存在，DPL=0，Type=非系统段，数据段，界限4G，可读写
     [KERNEL_DATA_SEG / 8] = {0xffff, 0x0000, 0x9200, 0x00cf},
-
     // 0x00cffa000000ffff - 从0地址开始，P存在，DPL=3，Type=非系统段，32位代码段，界限4G
-    [APP_CODE_SEG/ 8] = {0xffff, 0x0000, 0xfa00, 0x00cf},
+    [APP_CODE_SEG / 8] = {0xffff, 0x0000, 0xfa00, 0x00cf},
     // 0x00cff3000000ffff - 从0地址开始，P存在，DPL=3，Type=非系统段，数据段，界限4G，可读写
-    [APP_DATA_SEG/ 8] = {0xffff, 0x0000, 0xf300, 0x00cf},
+    [APP_DATA_SEG / 8] = {0xffff, 0x0000, 0xf300, 0x00cf},
+    // 两个进程的task0和tas1的tss段:自己设置，直接写会编译报错
+    [TASK0_TSS_SEL / 8] = {0x0068, 0, 0xe900, 0x0},
+    [TASK1_TSS_SEL / 8] = {0x0068, 0, 0xe900, 0x0},
+    // 系统调用的调用门
+    [SYSCALL_SEL / 8] = {0x0000, KERNEL_CODE_SEG, 0xec03, 0x0000},
 };
+
+// --------------------------------- Function Define ---------------------------------
 
 // outb: 向端口写入一个字节
 void outb(uint8_t data, uint16_t port)
 {
     // c调用汇编, 语法: __asm__ __volatile__("汇编指令" : : "寄存器"(变量));
-   __asm__ __volatile__("outb %[v], %[p]" : : [p]"d" (port), [v]"a" (data));
+    __asm__ __volatile__("outb %[v], %[p]"
+                         :
+                         : [p] "d"(port), [v] "a"(data));
 }
 
+void task_sched (void) {
+    static int task_tss = TASK0_TSS_SEL;
 
+    // 更换当前任务的tss，然后切换过去
+    task_tss = (task_tss == TASK0_TSS_SEL) ? TASK1_TSS_SEL : TASK0_TSS_SEL;
+    uint32_t addr[] = {0, task_tss };
+    __asm__ __volatile__("ljmpl *(%[a])"::[a]"r"(addr));
+}
+
+void do_syscall(int func, char *str, char color)
+{
+    static int row = 1;
+
+    if (func == 2)
+    {
+        // 显示器共80列，25行，按字符显示，每个字符需要用两个字节表示
+        unsigned short *dest = (unsigned short *)0xb8000 + 80 * row;
+        while (*str)
+        {
+            // 高位表示颜色，低位表示字符
+            *dest++ = *str++ | (color << 8);
+        }
+
+        // 逐行显示, 超过一行则回到第0行再显示
+        row = (row >= 25) ? 0 : row + 1;
+
+        // 加点延时, 让显示慢下来
+        for (int i = 0; i < 0xFFFFFF; i++)
+            ;
+    }
+}
+
+void sys_show(char *str, char color)
+{
+    // 使用特权级0
+    const unsigned long sys_gate_addr[] = {0, SYSCALL_SEL};
+
+    // 采用调用门, 这里只支持5个参数
+    // 用调用门的好处是会自动将参数复制到内核栈中，这样内核代码很好取参数
+    // 而如果采用寄存器传递，取参比较困难，需要先压栈再取
+    __asm__ __volatile__("push %[color];push %[str];push %[id];lcalll *(%[gate])\n\n" ::[color] "m"(color), [str] "m"(str), [id] "r"(2), [gate] "r"(sys_gate_addr));
+}
 
 /**
  * @brief 任务0
-*/
+ */
 void task_0(void)
 {
-    for (;;){}
+    char *str = "task a: 1234";
+    uint8_t color = 0;
+    for (;;)
+    {
+        sys_show(str, color++);
+    }
 }
 
-// timer_init: 初始化定时器, 函数声明
-void timer_init(void);
+void task_1(void)
+{
+    char *str = "task b: 5678";
+    uint8_t color = 0xff;
+    for (;;)
+    {
+        sys_show(str, color--);
+    }
+}
 
 void os_init(void)
 {
@@ -102,6 +233,11 @@ void os_init(void)
     idt_table[0x20].offset_l = (uint32_t)timer_init & 0xffff;
     idt_table[0x20].selector = KERNEL_CODE_SEG;
     idt_table[0x20].attr = 0x8E00; // 存在，DPL=0, 中断门
+
+    // 添加任务和系统调用
+    gdt_table[TASK0_TSS_SEL / 8].base_l = (uint16_t)(uint32_t)task0_tss;
+    gdt_table[TASK1_TSS_SEL / 8].base_l = (uint16_t)(uint32_t)task1_tss;
+    gdt_table[SYSCALL_SEL / 8].limit_l = (uint16_t)(uint32_t)syscall_handler;
 
     // 虚拟内存
     // 0x80000000开始的4MB区域的映射
--- diy-200lines-os/b00.00/source/os.h
+++ diy-200lines-os/b00.00/source/os.h
@@ -15,6 +15,9 @@
 #define KERNEL_DATA_SEG     (2 * 8)
 #define APP_CODE_SEG        (3 * 8 | 3)                 // 3: 用户态, 特权级为3
 #define APP_DATA_SEG        (4 * 8 | 3)                 // 3: 用户态, 特权级为3
+#define TASK0_TSS_SEL       (5 * 8)                     // 任务0的TSS选择子
+#define TASK1_TSS_SEL       (6 * 8)                     // 任务1的TSS选择子
+#define SYSCALL_SEL         (7 * 8)                     // 系统调用选择子
 
 
 #endif // OS_H
